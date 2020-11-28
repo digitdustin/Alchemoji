360e6c522e1f4498ec6dfcf515532709ba99a39e
Dustin Karp <dustinkarp52@gmail.com>
2020-11-27 20:02:05 -0500

    improved element movement

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 //Selectors
 const elementList = document.querySelector('.element-list');
 const workspace = document.querySelector('.workspace');
-const element = document.querySelector('.element');
+//const element = document.querySelector('.element');
+var allElements = document.querySelectorAll('.element-img');
 
 
 //Button Listeners
 elementList.addEventListener('click', addElement);
-element.addEventListener('mousedown', drag, true);
+//element.addEventListener('mousedown', drag, true);
+allElements.forEach((elem) => {
+    elem.addEventListener('mousedown', drag, true);
+});
 
 
 //Functions
-function addElement(event) {
+function addElement(event, elem) {
     event.preventDefault();
     //create new element div
     const element = document.createElement("div");
@@ -18,7 +22,7 @@ function addElement(event) {
     //create new element emoji li
     const elementImg = document.createElement("li");
     elementImg.classList.add("element-img");
-    elementImg.innerText = "❔";
+    elementImg.innerText = elem.innerText;
     element.appendChild(elementImg);
     //create new element text li
     const elementTxt = document.createElement("h1");
@@ -29,23 +33,30 @@ function addElement(event) {
     elementList.appendChild(element);
 }
 
+//moving element out of list into workspace
 function drag(event) {
     console.log('drag start');
     event.preventDefault();
     var mousePosition;
-    var div = document.createElement("div");
-    var offset = [0,0];
+    
+    var offset = [
+        event.clientX,
+        event.clientY
+    ];
     var isDown = true;
 
     //create new div
-    //div = document.createElement("div");
+    
+    var div = document.createElement("div");
     div.style.position = "absolute";
     div.classList.add('element-img');
-    div.style.left = 0;
-    div.style.top = 0;
+    div.style.left = event.clientX - 20 + "px";
+    div.style.top = event.clientY - 20 + "px";
     div.style.width = "40px";
-    div.innerText = "🔥";
+    div.innerText = this.innerText;
     div.style.zIndex = 2;
+
+    
 
     workspace.appendChild(div);
 
@@ -60,8 +71,54 @@ function drag(event) {
                 x : e.clientX,
                 y : e.clientY
             };
-            div.style.left = (mousePosition.x + offset[0]) + 'px';
-            div.style.top = (mousePosition.y + offset[1]) + 'px';
+            div.style.left = (mousePosition.x - 20) + 'px';
+            div.style.top = (mousePosition.y - 20) + 'px';
+        }
+    }, true);
+
+    div.addEventListener('mousedown', move, true);
+}
+
+//for moving element in workspace
+function move(event) {
+    console.log('move start');
+    event.preventDefault();
+    var mousePosition;
+    
+    var offset = [
+        event.clientX,
+        event.clientY
+    ];
+    var isDown = true;
+
+    //create new div
+    
+    var div = this;
+    div.style.position = "absolute";
+    div.classList.add('element-img');
+    div.style.left = event.clientX - 20 + "px";
+    div.style.top = event.clientY - 20 + "px";
+    div.style.width = "40px";
+    div.innerText = this.innerText;
+    div.style.zIndex = 2;
+
+    
+
+    workspace.appendChild(div);
+
+    document.addEventListener('mouseup', () => {
+        isDown = false;
+    }, true);
+
+    document.addEventListener('mousemove', (e) => {
+        e.preventDefault();
+        if (isDown) {
+            mousePosition = {
+                x : e.clientX,
+                y : e.clientY
+            };
+            div.style.left = (mousePosition.x - 20) + 'px';
+            div.style.top = (mousePosition.y - 20) + 'px';
         }
     }, true);
 }
